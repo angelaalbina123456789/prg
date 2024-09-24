1316acc6c34a8abbf074b914212b3b164f9235d6
albina <shermatalieva06@gmail.com>
2024-09-24 15:32:55 +0700

    Добавлен оператор break

--- pr003.java
+++ pr003.java
@@ -140,8 +140,11 @@ class pr003 {
 		}
 		
 		//Бесконечный цикл:
-		//System.out.println("\n");
-		//for (;;);
+		System.out.println("\n");
+		for (;;){
+			ch = (char) System.in.read();
+			if(ch == 'q') break;
+		}
 		
 		System.out.println("\n");
 
@@ -187,5 +190,31 @@ class pr003 {
 			} while (ignore != '\n');
 		} while (ch != 'q');
 
+		//Оператор break
+		
+		System.out.println("\n");
+
+                for (x1 = 100; x1 > -100; x1 -= 5) {
+                        System.out.println(x1);
+			if (x1 == 50) break; //прекращаем выполнение цикла
+
+		}
+
+		System.out.println("\n");
+		
+		//break c вложенными циклами
+		for ( int i9=0; i9<3; i9++) {
+			System.out.println("Счетчик внешнего цикла: " + i9);
+			System.out.println("Сячетчик внутреннего цикла: ");
+			int t = 0;
+			while (t < 100) {
+				if(t==10) break;
+				System.out.print(t + " ");
+				t++;
+			}
+			System.out.println();
+		}
+		System.out.println("Циклы закончились");
+	
 	}
 }
