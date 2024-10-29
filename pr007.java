9f5648a60a9f756ac39d66d521136b143ea9786d
albina <shermatalieva06@gmail.com>
2024-10-29 15:44:55 +0700

    добавили доступ к закрытым членам класса

--- pr007.java
+++ pr007.java
@@ -1,4 +1,44 @@
+//управление доступом к членам класса
+class Mod {
+	private int alpha; //закрытая переменная
+		public int beta; //открытая переменная
+		int gamma; //переменная с доступа по умолчанию
+		void setAlpha(int a) {
+			alpha = a;
+		}
+		int getAlpha() {
+			return alpha;
+		}
+}
+class FailSoftArray {
+	private int[] a;
+	private int errval;
+	public int length;
 
+	public FailSoftArray(int size, int errv) {
+		a = new int[size];
+		errval = errv;
+		length = size;
+	}
+
+	public int get(int index) {
+		if(indexOK(index))
+			return a[index];
+		return errval;
+	}
+	public boolean put(int index, int val) {
+		if(indexOK(index)) {
+			a[index] = val;
+			return true;
+		}
+		return false;
+	}
+	private boolean indexOK(int index) {
+		if(index >= 0 & index < length)
+			return true;
+		return false;
+	}
+}
 class pr007 {
 	public static void main(String[] args) {
 		Mod ob = new Mod();
@@ -6,7 +46,7 @@ class pr007 {
 		ob.setAlpha(77);
 		System.out.println("Значение ob.alpha: " +  ob.getAlpha());
 		// прямой доступ к переменной заперщен: ob.alpha = 100;
-		ob.betAa = 242;
+		ob.beta = 242;
 		ob.gamma = 34;
 		//пример использования класса отказ устойчивог поведения массива
 		System.out.println();
@@ -18,7 +58,7 @@ class pr007 {
 		for(int i=0; i<(fs.length * 2); i++) 
 			fs.put(i, i*10);
 
-		for(int i=0; i < (fs.lenth * 2); i++) {
+		for(int i=0; i < (fs.length * 2); i++) {
 			x = fs.get(i);
 			if(x != -999) System.out.println(x + " ");
 		}
@@ -30,11 +70,11 @@ class pr007 {
                         if(!fs.put(i, i*10))
 				System.out.println("Индекс " + i + " выходит за границы массива");
 
-                for(int i=0; i < (fs.lenth * 2); i++) {
+                for(int i=0; i < (fs.length * 2); i++) {
                         x = fs.get(i);
                         if(x != -999) System.out.println(x + " ");
 			else 
-				System.out.println("индекс " + i + " выходт за границы");
+				System.out.println("индекс " + i + " выходит за границы массива");
                 }
                 System.out.println();
 	}
